0141a172c5935c1d5008190abef2fa5eb7e395d7
Lautaro <21052007Lau@gmail.com>
2024-05-06 17:25:01 -0300

    Update ejer4.py

--- Unidad 2 Python/ejer4.py
+++ Unidad 2 Python/ejer4.py
@@ -1,1 +1,9 @@
-def 
+def superficie():
+    lado1 = int(input("Ingrese la altura en enteros "))
+    lado2 = float(input("Ingrese la base en decimal "))
+    
+    super = lado1 * lado2
+
+    print("La superficie es:", super)
+
+superficie()    
