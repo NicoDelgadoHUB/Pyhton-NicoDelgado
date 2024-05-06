5b1eb5f2b09f53df47c22db1f9e699729af30c48
Lautaro <21052007Lau@gmail.com>
2024-05-06 17:12:44 -0300

    Create ejerc3.py

---
+++ Unidad 2 Python/ejerc3.py
@@ -0,0 +1,9 @@
+def superficie():
+    lado1 = int(input("Ingrese el valor de la altura "))
+    lado2 = int(input("Ingrese el valor de la base "))
+
+    super = lado1 * lado2
+
+    print("La superficie del rectangulo es:", super)
+
+superficie()    
