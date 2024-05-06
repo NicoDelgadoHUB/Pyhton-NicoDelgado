613bbe61f6604aaf230dc6c405198a2fb311b1cf
Lautaro <21052007Lau@gmail.com>
2024-05-06 17:12:25 -0300

    Create ejer1.py

---
+++ Unidad 2 Python/ejer1.py
@@ -0,0 +1,14 @@
+#n1 = int(input("Ingrese un numero"))
+#print(n1)
+
+
+def suma():
+    n1 = int(input("Ingrese un numero entero "))
+    n2 = int(input("Ingrese otro numero entero "))
+
+    resultado = n1 + n2
+
+    print("El resultado es:", resultado)
+
+suma()
+    
