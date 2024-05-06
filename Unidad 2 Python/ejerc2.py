130ba7c30330e583296428c80a4c18da5a8d666c
Lautaro <21052007Lau@gmail.com>
2024-05-06 17:12:39 -0300

    Create ejerc2.py

---
+++ Unidad 2 Python/ejerc2.py
@@ -0,0 +1,10 @@
+def suma():
+    n1 = int(input("Ingrese un numero entero "))
+    n2 = int(input("Ingrese um segundo numero "))
+    n3 = int(input("Ingrese un tercer numero "))
+    n4 = int(input("Ingrese un ultimo numero "))
+
+    resultado = n1 + n2 + n3 + n4
+    print("El resultado es:", resultado)
+
+suma()    
