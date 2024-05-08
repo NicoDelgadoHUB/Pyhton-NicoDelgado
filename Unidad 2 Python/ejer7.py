89644e0385e2d955ddcfb4ca1ddf6daa75adab19
NicoDelgadoHUB <ncklsdelgado@gmail.com>
2024-05-08 15:04:26 -0300

    Create ejer7.py

---
+++ Unidad 2 Python/ejer7.py
@@ -0,0 +1,15 @@
+def promedio_seis():
+    n1 = float(input("Ingrese un numero "))
+    n2 = float(input("Ingrese un segundo numero "))
+    n3 = float(input("Ingrese un tercer numero "))
+    n4 = float(input("Ingrese un cuarto numero "))
+    n5 = float(input("Ingrese un quinto numero "))
+    n6 = float(input("Ingrese un ultimo numero "))
+
+    suma = n1 + n2 + n3 + n4 + n5 + n6
+
+    promedio = 6 / suma
+
+    print("El promedio es:", promedio)
+
+promedio_seis()    
