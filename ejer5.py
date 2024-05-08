c8745607dc0c7988f2337ec3e036cdad04772321
NicoDelgadoHUB <ncklsdelgado@gmail.com>
2024-05-08 14:16:01 -0300

    Delete ejer5.py

--- ejer5.py
+++
@@ -1,9 +0,0 @@
-def segundos():
-    horas = int(input("Ingrese las horas: "))
-    minutos = int(input("Ingrese los minutos: "))
-    segundos = int(input("Ingrese los segundos: "))
-
-    total_segundos = horas * 3600 + minutos * 60 + segundos
-    print("El total de segundos es:", total_segundos)
-
-segundos()
