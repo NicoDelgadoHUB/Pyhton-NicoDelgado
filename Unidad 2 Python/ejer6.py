a01050c1a6c1c3a961d2bc2e992b4941d21d7f5f
NicoDelgadoHUB <ncklsdelgado@gmail.com>
2024-05-08 14:32:08 -0300

    Create ejer6.py

---
+++ Unidad 2 Python/ejer6.py
@@ -0,0 +1,9 @@
+def superficie_triangulo():
+    base = float(input("Ingrese la base del triángulo: "))  
+    altura = float(input("Ingrese la altura del triángulo: ")) 
+
+    superficie = 0.5 * base * altura  
+
+    print("La superficie del triángulo es:", superficie)
+
+superficie_triangulo()
